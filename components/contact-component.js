5ce828bfc95eb6ba82386200494754022db05549
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-10-14 14:55:40 -0600

    map

--- components/contact-component.js
+++ components/contact-component.js
@@ -4,14 +4,14 @@
             templateUrl: 'components/contact-component.html',
           
         })
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(+47.6136732,-122.3361331),
-    zoom: 17,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-google.maps.event.addDomListener(window, 'load', initialize);
-}
+// function initialize() {
+//   var mapProp = {
+//     center:new google.maps.LatLng(+47.6136732,-122.3361331),
+//     zoom: 17,
+//     mapTypeId:google.maps.MapTypeId.ROADMAP
+//   };
+//   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+// google.maps.event.addDomListener(window, 'load', initialize);
+// }
 
 }());
